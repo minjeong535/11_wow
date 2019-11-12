4fb0a588300090bf536b1838faa0c7e4dc5caa5f
minjeong535 <55127421+minjeong535@users.noreply.github.com>
2019-11-12 15:44:43 +0900

    3

--- main.c
+++ main.c
@@ -4,19 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char *pc;
-	int *pi;
-	double *pd;
-	
-	pc=(char *)10000;
-	pi=(int*)10000;
-	pd=(double *)10000;
-	
-	printf("증가 전: pc=%d, pi= %d, pd= %d\n", pc, pi, pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	printf("증가 후: pc= %d, pi= %d, pd= %d\n", pc, pi, pd);
+
+char buffer[8];
+double *pd;
+int *pi;
+
+pd=(double *)buffer;
+*pd=3.14;
+printf("%f\n", *(double*)buffer);
+
+pi=(int*)buffer;
+*pi=123;
+*(pi+1)=456;
+printf("%d %d\n", *(int*)buffer, *((int*)buffer+1));
 	return 0;
 }
